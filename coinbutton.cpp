95921b370c3f1d1d713a75c7ba8810eae1297969
zycgy <314981@whut.edu.cn>
2025-05-07 00:48:51 +0800

    金币按钮

--- coinbutton.cpp
+++ coinbutton.cpp
@@ -1,5 +1,5 @@
 #include "coinbutton.h"
-
+#include <QTimer>
 
 CoinButton::CoinButton(QString coinBtnPath)
 {
@@ -10,4 +10,57 @@ CoinButton::CoinButton(QString coinBtnPath)
     this->setIcon(pix);
     this->setIconSize(QSize(pix.width(),pix.height()));
     this->setStyleSheet("QPushButton{border:0px}");//无边框
+
+    timer1 = new QTimer(this);
+    timer2 = new QTimer(this);
+    connect(timer1,&QTimer::timeout,[=](){
+        QPixmap pix;
+        QString str = QString(":/res/Coin000%1.png").arg(min++);
+        pix.load(str);
+        pix = pix.scaled(pix.width() * 1.4, pix.height() * 1.4);
+        this->setFixedSize(pix.width(),pix.height());
+        this->setIcon(pix);
+        this->setIconSize(QSize(pix.width(),pix.height()));
+        this->setStyleSheet("QPushButton{border:0px}");//无边框
+        isChange = true;
+        if(min>max)
+        {
+            min = 1;
+            timer1->stop();
+            isChange = false;
+        }
+    });
+
+    connect(timer2,&QTimer::timeout,[=](){
+        QPixmap pix;
+        QString str = QString(":/res/Coin000%8.png").arg(max--);
+        pix.load(str);
+        pix = pix.scaled(pix.width() * 1.4, pix.height() * 1.4);
+        this->setFixedSize(pix.width(),pix.height());
+        this->setIcon(pix);
+        this->setIconSize(QSize(pix.width(),pix.height()));
+        this->setStyleSheet("QPushButton{border:0px}");//无边框
+        isChange = true;
+        if(max<min)
+        {
+            max = 8;
+            timer2->stop();
+            isChange = false;
+        }
+    });
+}
+
+
+void CoinButton::changeCoin()
+{
+    if(flag)
+    {
+        flag = false;
+        timer1->start(50);
+    }
+    else
+    {
+        flag = true;
+        timer2->start(50);
+    }
 }
--- coinbutton.h
+++ coinbutton.h
@@ -9,6 +9,15 @@ class CoinButton : public QPushButton
 public:
     //explicit CoinButton(QWidget *parent = nullptr);
     CoinButton(QString coinBtnPath);
+    int x;
+    int y;
+    bool flag;
+    void changeCoin();
+    QTimer *timer1;
+    QTimer *timer2;
+    int min = 1;
+    int max = 8;
+    bool isChange = false;
 signals:
 };
 
